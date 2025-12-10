c5b32395b3058efb8f354610f807828eb747fe38
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2025-12-10 14:10:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 #include <vector>
 #include <string>
 #include <cmath>    // for std::round, std::pow
-
+/* Weapons class*/
+enum class Weapons{
+    Sword,
+    Shield,
+    Armor,
+    Bow
+};
+//Convert weapon enum to string
+static std::string WeaponString(Weapons equipment) {
+    switch (equipment) {
+        case Weapons::Sword  :     return "Sword";
+        case Weapons::Shield :     return "Shield";
+        case Weapons::Armor  :     return "Armor";
+        case Weapons::Bow    :     return "Bow";
+        default:  return "Unknown";
+    }
+}
 /* Player stats Class*/
 class Player_stats {
 private:
@@ -18,9 +34,31 @@ private:
     int nextlevelxp;    // xp needed to reach the next level from current level
     double Scaling;     // scaling factor for nextlevelxp
 
-public:
+    /* Weapons unlock */
+    std::vector<Weapons> unlock_weapons;
+    std::vector<std::pair<int,Weapons>> available_weapons = {
+        {1, Weapons::Sword}, 
+        {2, Weapons::Shield},
+        {3, Weapons::Bow},
+        {4, Weapons::Armor}
+    };
 
-    // Compute max xp for next level
+
+public:
+// Unlock weapons for current level
+    void weapons_unlock(){
+        for (const auto& [reqLevel,equip]: available_weapons){
+            if(reqLevel <= level){
+                if(std::find(unlock_weapons.begin(), unlock_weapons.end(),equip)==unlock_weapons.end()){
+                    unlock_weapons.push_back(equip);
+                    std::cout << PlayerName << " has reached level " << level << " and unlocked " 
+                                            << WeaponString(equip) << "\n";
+                }
+            }
+        }
+    }
+
+// Compute max xp for next level
     void max_xp_per_level() {
         int baseXP = 100;
         nextlevelxp = static_cast<int>(std::round(baseXP * std::pow(Scaling, std::max(0, level - 1))));
@@ -62,7 +100,7 @@ public:
         }
     }
 
-    // Player Constructor
+// Player Constructor
     Player_stats(const std::string& name,
                  int HP,
                  int Melee,
@@ -84,6 +122,7 @@ public:
           Scaling(xp_scaling){
                 if (level < 1) level = 1;
                 max_xp_per_level();
+                weapons_unlock();
         }
 
     // Display Player Stats
@@ -97,6 +136,17 @@ public:
                   << " | Def: " << Defense
                   << "\n";
     }
+    // Display Unlocked Weapons
+    void display_unlock(){
+        if(unlock_weapons.empty()){
+            std::cout << "No weapons unlocked yet.\n";
+            return;
+        }
+        for(i=0;i<unlock_weapons.size();i++){
+            std::cout << WeaponString(unlock_weapons[i]);
+            if( i+1 < unlock_weapons.size()) std::cout << ", ";
+        }
+    }
 };
 
 int main() {
@@ -107,9 +157,9 @@ int main() {
     player.Status();
 
     // Simulate gaining experience
-    player.To_next_level(150);
-
+    player.To_next_level(300);
     player.Status();
+    player.display_unlock();
 
     return 0;
 }
